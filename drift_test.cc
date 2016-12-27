d3d21763d23c4eaf22e2d32ce7b351652d886bf2
Racinettee <Racinettee@outlook.com>
2016-12-27 15:43:37 -0800

    Remove unused  lines of code

--- drift_test.cc
+++ drift_test.cc
@@ -1,28 +1,16 @@
+#include <memory>
 #include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
-#include <stdexcept>
-#include <deque>
-#include <new>
-#include <locale>
-#include <codecvt>
-#include <initializer_list>
 using namespace std;
 
+#include "context.hh"
 #include "variant.hh"
 #include "private/types.hh"
 
 using namespace drift;
 
-#include "context.hh"
-
 int main() try
 {
-  using namespace std;
-
   context ctxt;
-
   shared_ptr<variant> result = ctxt(L"16 + 16");
   cout << result->num << endl;
 }
