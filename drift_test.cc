e0fe9b2f3db5faa224dca23b752392aadd8812be
Racinettee <Racinettee@outlook.com>
2016-12-30 23:53:30 -0800

    This test works

--- drift_test.cc
+++ drift_test.cc
@@ -12,9 +12,10 @@ int main() try
   context ctxt;
   shared_ptr<variant> result = ctxt(
     L"-2.3*2\n"
-    L"let init = 10");
+    L"let init = -(10* -2)");
   cout << result->num << endl;
-  wcout << kind_string(ctxt[L"init"]) << L": "<< ctxt[L"init"]->num << endl;
+  variant_ptr init = ctxt[L"init"];
+  wcout << kind_string(init) << L": "<< init->num << endl;
 }
 catch(std::exception& e)
 {
